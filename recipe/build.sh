f79ec6042fb9e4d4b520fd7748b9555de87a8560
Jack Olivieri <jolivieri@anaconda.com>
2024-02-23 20:57:18 +0100

    fix patching in build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,4 @@
 wheel unpack ${PKG_NAME}-${PKG_VERSION}-py3-none-any.whl
-git apply ${RECIPE_DIR}/0001-remove-unused-dependency.patch
+patch --binary -p1 ./${PKG_NAME}-${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.dist-info/METADATA < $RECIPE_DIR/0001-remove-unused-dependency.patch
 wheel pack ${PKG_NAME}-${PKG_VERSION}
 $PYTHON -m pip install -vv ${PKG_NAME}-${PKG_VERSION}-py3-none-any.whl
