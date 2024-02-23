9cc9282ef95741b728551987af085d208a713496
Jack Olivieri <jolivieri@anaconda.com>
2024-02-23 21:00:08 +0100

    add build script for win

---
+++ recipe/build.bat
@@ -0,0 +1,4 @@
+wheel unpack %PKG_NAME%-%PKG_VERSION%-py3-none-any.whl
+patch --binary -p1 .\%PKG_NAME%-%PKG_VERSION%\%PKG_NAME%-%PKG_VERSION%.dist-info/METADATA < %RECIPE_DIR%\0001-remove-unused-dependency.patch
+wheel pack %PKG_NAME%-%PKG_VERSION%
+%PYTHON% pip install -vv %PKG_NAME%-%PKG_VERSION%-py3-none-any.whl
