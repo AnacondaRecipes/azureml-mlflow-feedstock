099a3605fccf0dd12b9fb7cccfc353e0f31bc845
Jack Olivieri <jolivieri@anaconda.com>
2024-02-26 15:36:38 +0100

    fix win build script

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,4 +1,4 @@
 wheel unpack %PKG_NAME%-%PKG_VERSION%-py3-none-any.whl
 patch --binary -p1 .\%PKG_NAME%-%PKG_VERSION%\%PKG_NAME%-%PKG_VERSION%.dist-info/METADATA < %RECIPE_DIR%\0001-remove-unused-dependency.patch
 wheel pack %PKG_NAME%-%PKG_VERSION%
-%PYTHON% pip install -vv --no-deps --no-build-isolation %PKG_NAME%-%PKG_VERSION%-py3-none-any.whl
+%PYTHON% -m pip install -vv --no-deps --no-build-isolation %PKG_NAME%-%PKG_VERSION%-py3-none-any.whl
