3d6a7ef7001f4741a60da1e203eb5e995f595eaf
Jack Olivieri <jolivieri@anaconda.com>
2024-02-27 19:35:37 +0100

    do not deviate from upstream requirements

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,1 @@
-wheel unpack %PKG_NAME%-%PKG_VERSION%-py3-none-any.whl
-patch --binary -p1 .\%PKG_NAME%-%PKG_VERSION%\%PKG_NAME%-%PKG_VERSION%.dist-info/METADATA < %RECIPE_DIR%\0001-remove-unused-dependency.patch
-wheel pack %PKG_NAME%-%PKG_VERSION%
 %PYTHON% -m pip install -vv --no-deps --no-build-isolation %PKG_NAME%-%PKG_VERSION%-py3-none-any.whl
